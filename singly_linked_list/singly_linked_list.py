e571ff23bbe0638dd8082397915d4e4e056b9c43
Anthony N <foto.arn@gmail.com>
2020-09-08 11:10:31 -0700

    day 1 mvp

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self, length=0):
+        self.head = None
+        self.tail = None
+        self.length = length
+
+    def add_to_tail(self, value):
+        newNode = Node(value, None)
+        self.length += 1
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+
+        else:
+            oldTail = self.tail
+            oldTail.next = newNode
+            self.tail = newNode
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        else:
+            self.length -= 1
+            if self.head == self.tail:
+                oldHead = self.head
+                self.head = None
+                self.tail = None
+                return oldHead.value
+            else:
+                currentHead = self.head
+                self.head = currentHead.next
+                return currentHead.value
+
+    def remove_tail(self):
+        if self.head == None:
+            return None
+
+        elif self.head == self.tail:
+            self.length -= 1
+            oldTail = self.tail
+            self.head = None
+            self.tail = None
+            return oldTail.value
+
+        else:
+            self.length -= 1
+            current = self.head
+            oldTail = self.tail
+            while current.next is not self.tail:
+                current = current.next
+
+            removedValue = self.tail.value
+            self.tail = None
+            self.tail = current
+            return removedValue
